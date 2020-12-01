89e952d06ed39d7bec45bd841af883dd0fc05c8b
Max Patiiuk <max@patii.uk>
2020-12-01 15:01:13 +0000

    chore: update legacy URLs and usernames

--- config/required.php
+++ config/required.php
@@ -20,7 +20,7 @@ if(CONFIGURATION==='localhost'){
 	# Set this to an empty folder
 	# Make sure the web server has write permissions to this folder
 	# **Warning!** All of the files present in this directory would be deleted
-	define('WORKING_LOCATION','/Users/mambo/Downloads/taxa_tree_stats/');
+	define('WORKING_LOCATION','/Users/maxpatiiuk/Downloads/taxa_tree_stats/');
 
 }
 
